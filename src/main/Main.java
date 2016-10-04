2d72f46678890ae3733afba834909182e9b3e8d8
barbiero <pedrobarbiero@gmail.com>
2016-10-03 21:44:10 -0300

    jfuzzylogic exemplo

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,10 +1,35 @@
 package main;
 
+import net.sourceforge.jFuzzyLogic.FIS;
+import net.sourceforge.jFuzzyLogic.plot.JFuzzyChart;
+import net.sourceforge.jFuzzyLogic.rule.Variable;
 
 public class Main {
 	
 	static public void main(String[] args) {
-		System.out.println("Hello, World!");
+		// Carregar arquivo FCL
+		String filename = "fcl/tipper.fcl";
+		FIS fis = FIS.load(filename, true);
+		
+		if(fis == null) {
+			System.err.println("Arquivo não pode ser aberto: " + filename);
+			return;
+		}
+		
+		
+		//Mostrar
+		JFuzzyChart.get().chart(fis);
+		
+		fis.setVariable("service", 3);
+		fis.setVariable("food", 7);
+		
+		fis.evaluate();
+		
+		Variable tip = fis.getVariable("tip");
+		JFuzzyChart.get().chart(tip, tip.getDefuzzifier(), true);
+		
+		System.out.println(fis);
+		
 	}
 
 }
